05d584f89e6d10f61041202cc7a4526c3f8d971f
KikiLeKiri <116727828+KikiLeKiri@users.noreply.github.com>
2023-12-13 22:02:52 +0100

    log et sing

--- Les_codes/Test_client_grph.py
+++ Les_codes/Test_client_grph.py
@@ -1,7 +1,67 @@
 import sys
-import socket
 import threading
+import socket
 from PyQt6.QtWidgets import *
+
+
+class LoginWidget(QWidget):
+    def __init__(self, parent, client_gui):
+        super().__init__()
+
+        self.client_gui = client_gui
+
+        self.username_input = QLineEdit(self)
+        self.password_input = QLineEdit(self)
+        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+
+        self.login_button = QPushButton("Se Connecter", self)
+        self.login_button.clicked.connect(self.client_gui.show_chat)
+        
+        self.signup_button = QPushButton("Créer un Compte", self)
+        self.signup_button.clicked.connect(self.show_signup)
+
+        self.quit_button = QPushButton("Quitter", self)
+        self.quit_button.clicked.connect(self.client_gui.close)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(QLabel("Nom d'utilisateur:"))
+        layout.addWidget(self.username_input)
+        layout.addWidget(QLabel("Mot de passe:"))
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.login_button)
+        layout.addWidget(self.signup_button)
+        layout.addWidget(self.quit_button)
+
+    def show_signup(self):
+        self.client_gui.stacked_widget.setCurrentIndex(1)
+
+
+class SignupWidget(QWidget):
+    def __init__(self, parent, client_gui):
+        super().__init__()
+
+        self.client_gui = client_gui
+
+        self.username_input = QLineEdit(self)
+        self.password_input = QLineEdit(self)
+        self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+
+        self.signup_button = QPushButton("Créer un Compte", self)
+        self.signup_button.clicked.connect(self.client_gui.show_chat)
+
+        self.back_button = QPushButton("Retour", self)
+        self.back_button.clicked.connect(self.show_login)
+
+        layout = QVBoxLayout(self)
+        layout.addWidget(QLabel("Nom d'utilisateur:"))
+        layout.addWidget(self.username_input)
+        layout.addWidget(QLabel("Mot de passe:"))
+        layout.addWidget(self.password_input)
+        layout.addWidget(self.signup_button)
+        layout.addWidget(self.back_button)
+
+    def show_login(self):
+        self.client_gui.stacked_widget.setCurrentIndex(0)
 
 
 class ClientGUI(QWidget):
@@ -9,22 +69,42 @@ class ClientGUI(QWidget):
         super().__init__()
 
         self.setWindowTitle("Client")
-        self.setGeometry(100, 100, 400, 300)
+        self.setGeometry(100, 100, 1000, 800)
 
-        self.text_display = QTextEdit(self)
+        self.stacked_widget = QStackedWidget(self)
+        self.login_widget = LoginWidget(self, self)
+        self.signup_widget = SignupWidget(self, self)
+        self.chat_widget = QWidget(self)
+        self.chat_layout = QVBoxLayout(self.chat_widget)
+        self.text_display = QTextEdit(self.chat_widget)
         self.text_display.setReadOnly(True)
-
-        self.input_box = QLineEdit(self)
-
-        self.send_button = QPushButton("Envoyer", self)
+        self.input_box = QLineEdit(self.chat_widget)
+        self.send_button = QPushButton("Envoyer", self.chat_widget)
         self.send_button.clicked.connect(self.send_message)
-
-        layout = QVBoxLayout(self)
-        layout.addWidget(self.text_display)
-        layout.addWidget(self.input_box)
-        layout.addWidget(self.send_button)
+        self.chat_layout.addWidget(self.text_display)
+        self.chat_layout.addWidget(self.input_box)
+        self.chat_layout.addWidget(self.send_button)
+        self.stacked_widget.addWidget(self.login_widget)
+        self.stacked_widget.addWidget(self.signup_widget)
+        self.stacked_widget.addWidget(self.chat_widget)
 
         self.server_address = ('127.0.0.1', 8864)
+        self.client_socket = None
+        self.receive_thread = None
+
+        self.show_login()
+
+    def show_login(self):
+        self.stacked_widget.setCurrentIndex(0)
+
+    def show_signup(self):
+        self.stacked_widget.setCurrentIndex(1)
+
+    def show_chat(self):
+        self.stacked_widget.setCurrentIndex(2)
+        self.connect_to_server()
+
+    def connect_to_server(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
@@ -33,7 +113,7 @@ class ClientGUI(QWidget):
             self.receive_thread = threading.Thread(target=self.receive_messages)
             self.receive_thread.start()
         except Exception as e:
-            self.text_display.append(f"Erreur de connexion au serveur: {str(e)}")
+            self.text_display.append(f"Erreur de connexion au serveur")
 
     def send_message(self):
         message = self.input_box.text()
@@ -46,7 +126,7 @@ class ClientGUI(QWidget):
                 message = self.client_socket.recv(1024).decode()
                 self.text_display.append(message)
             except Exception as e:
-                self.text_display.append(f"Erreur de réception de message: {str(e)}")
+                self.text_display.append(f"Erreur de réception de message")
                 break
 
 
